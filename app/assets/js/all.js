7b7ee2432114bf54f468d48e4c4329feb965d6db
waynechen <n0918679182@gmail.com>
2022-07-20 14:56:46 +0800

    新增影片列表頁面&RWD

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -43,3 +43,48 @@ logout.addEventListener('click', function () {
 loginClose.addEventListener('click', function () {
   document.getElementById("login-container").style.top = "-120%";
 }); 
+
+
+
+
+// 廣告自動撥放及左右滑動
+var swiper = new Swiper(".ad-slider", {
+  loop:true,
+  grabCursor:true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// 影片清單分類按鈕
+let tags = document.querySelectorAll('.tag');
+tags.forEach(function(tag){
+  tag.addEventListener('click', ()=>{
+    tags.forEach(function(t){
+      t.classList.remove('active');
+    });
+    tag.classList.toggle('active');
+  });
+});
+
+
+
+// 影片清單分類按鈕 - RWD側邊選單
+let tagsBT = document.querySelector('.tags-rwd');
+let tagList = document.querySelector('.tags');
+let closeBT = document.querySelector('.close-bt');
+let cover = document.querySelector('.cover');
+tagsBT.addEventListener('click',()=>{
+  tagList.classList.toggle('active');
+  cover.classList.toggle('active');
+})
+closeBT.addEventListener('click',()=>{
+  tagList.classList.remove('active');
+  cover.classList.remove('active');
+})
