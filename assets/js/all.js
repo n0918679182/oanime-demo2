0ba4083e2912a9cb73ea51037b1f0050b789399d
waynechen <n0918679182@gmail.com>
2022-07-20 14:54:03 +0800

    Update 2022-07-20T06:53:58.964Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -35,5 +35,41 @@ logout.addEventListener('click', function () {
 });
 loginClose.addEventListener('click', function () {
   document.getElementById("login-container").style.top = "-120%";
+}); // 廣告自動撥放及左右滑動
+
+var swiper = new Swiper(".ad-slider", {
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+}); // 影片清單分類按鈕
+
+var tags = document.querySelectorAll('.tag');
+tags.forEach(function (tag) {
+  tag.addEventListener('click', function () {
+    tags.forEach(function (t) {
+      t.classList.remove('active');
+    });
+    tag.classList.toggle('active');
+  });
+}); // 影片清單分類按鈕 - RWD側邊選單
+
+var tagsBT = document.querySelector('.tags-rwd');
+var tagList = document.querySelector('.tags');
+var closeBT = document.querySelector('.close-bt');
+var cover = document.querySelector('.cover');
+tagsBT.addEventListener('click', function () {
+  tagList.classList.toggle('active');
+  cover.classList.toggle('active');
+});
+closeBT.addEventListener('click', function () {
+  tagList.classList.remove('active');
+  cover.classList.remove('active');
 });
 //# sourceMappingURL=all.js.map
